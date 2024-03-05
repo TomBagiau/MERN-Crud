770a57f8d1903dd2b30228bb0315cdfed437ce17
Tom <39921579+TomBagiau@users.noreply.github.com>
2024-03-05 12:39:05 -0500

    routes crud

--- server/server.js
+++ server/server.js
@@ -5,16 +5,86 @@ if(process.env.NODE_ENV != 'production'){
 
 const express = require('express');
 const dbConnect = require('./config/dbConnect');
+const Note = require('./models/note');
 
 //Create express app
 const app = express();
 
-//connect to Db
+//Configure express
+app.use(express.json())
+
+//Connect to Db
 dbConnect()
 
 //Routing 
 app.get('/', (req, res) => {
     res.json({ hello: 'world' });
+})
+
+app.get('/notes', async (req, res) => {
+    //find notes using mongoose
+    const notes = await Note.find()
+
+    //respond with them
+    res.json({ notes: notes })
+})
+
+app.get('/notes/:id', async (req, res) => {
+    //get id of the URL
+    const noteId = req.params.id
+
+    //Find the note using that id 
+    const note = await Note.findById(noteId)
+
+    //Respond with the note
+    res.json({note: note})
+})
+
+app.post('/notes', async (req, res) => {
+    //Get the sent in data off request body
+    const title = req.body.title
+    const body = req.body.body
+
+    //Create a note with it 
+    const note = await Note.create({
+        title: title,
+        body: body,
+    })
+
+    //respond with the note
+    res.json({note: note})
+})
+
+app.put('/notes/:id', async (req, res) => {
+    //Get the id of the url 
+    const noteId = req.params.id
+
+    //Get datas of request body 
+    const title = req.body.title
+    const body = req.body.body
+
+    //Find and update 
+    await Note.findByIdAndUpdate(noteId, {
+        title: title,
+        body: body,
+    })
+
+    //Find updated note
+    const note = await Note.findById(noteId)
+
+    //Respond with it
+    res.json({ note: note })
+})
+
+app.delete('/notes/:id', async (req, res) => {
+    //Get the id of the url 
+    const noteId = req.params.id
+
+    //delete the note
+    await Note.deleteOne({ _id: noteId })
+
+    //respond
+    res.json({ success: "Note deleted" })
 })
 
 //Start server 
